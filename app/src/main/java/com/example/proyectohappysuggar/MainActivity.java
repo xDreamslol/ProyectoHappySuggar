9a2b4c32101f61c3910095772e3f197efc288202
Ana <annitaovalle94@gmail.com>
2022-04-17 05:27:55 -0500

    diseño

--- app/src/main/java/com/example/proyectohappysuggar/MainActivity.java
+++ app/src/main/java/com/example/proyectohappysuggar/MainActivity.java
@@ -3,25 +3,62 @@ package com.example.proyectohappysuggar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.view.inputmethod.InputMethodManager;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnEntrar;
+
+    private EditText et_nombre;
+    private MediaPlayer mp, mp_great, mp_bad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btnEntrar = findViewById(R.id.btnIngresar);
-        btnEntrar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, EncuestaActivity1.class);
-                startActivity(intent);
-            }
-        });
+
+        et_nombre=findViewById(R.id.txt_nombre);
+
+        mp = MediaPlayer.create(this, R.raw.goats);
+        mp.start();
+        mp.setLooping(true);
+
+        mp_great = MediaPlayer.create(this, R.raw.wonderful);
+        mp_bad = MediaPlayer.create(this, R.raw.bad);
+
     }
+
+    public void Continuar(View view){
+        String nombre=et_nombre.getText().toString();
+        if (!nombre.equals("")) {
+
+
+            Intent intent = new Intent(MainActivity.this, EncuestaActivity1.class);
+            intent.putExtra("Participante", nombre);
+            mp_great.start();
+            startActivity(intent);
+            finish();
+        }else {
+            Toast.makeText(this,"primero debe escribir su nombre",Toast.LENGTH_SHORT).show();
+            mp_bad.start();
+            et_nombre.requestFocus();
+            InputMethodManager imm = (InputMethodManager)getSystemService(this.INPUT_METHOD_SERVICE);
+            imm.showSoftInput(et_nombre, InputMethodManager.SHOW_IMPLICIT);
+
+            mp.stop();
+            mp.release();
+        }
+    }
+    @Override
+    public void onBackPressed(){
+    }
+
+
+
 }

c1f551b666f871b1bdcf9484c2caf46e36176ec2
xdreamslol <gabrielcontreras0598@gmail.com>
2022-04-16 20:23:45 -0500

    Se agrega la pantalla inicio con menu desplegable.

--- app/src/main/java/com/example/proyectohappysuggar/MainActivity.java
+++ app/src/main/java/com/example/proyectohappysuggar/MainActivity.java
@@ -4,10 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Método para entrar a la encuesta
 
     Button btnEntrar;
 
@@ -23,5 +27,27 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    //Método para mostar y ocultar el menú en inicio
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.inicio, menu);
+        return true;
+    }
+
+    //Metodo para asignar las fuciones correspondientes a las opciones del menu.
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+
+        if(id == R.id.item1) {
+
+        } else if (id == R.id.item2){
+
+        } else if (id == R.id.item3){
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
